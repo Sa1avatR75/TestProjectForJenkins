829149ae71dc4e5274af1d433eb0288d291be02a
Salavat A <sa.amingaliev@gmail.com>
2024-09-23 20:10:29 -0500

    commit 1

---
+++ src/test/java/ReverseStringTests.java
@@ -0,0 +1,28 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReverseStringTests {
+    @Test
+    public void reverseTestWithNull(){
+        Assert.assertNull(ReverseString.reverseString(null));
+    }
+
+    @Test
+    public void reverseStringDigit(){
+        Assert.assertEquals("cba321", ReverseString.reverseString("123abc"));
+    }
+
+    @Test
+    public void reverseString1(){
+        Assert.assertEquals("elppa", ReverseString.reverseString("apple"));
+    }
+
+    @Test
+    public void reverseString2(){
+        Assert.assertEquals("elp3pa", ReverseString.reverseString("apple"));
+    }
+
+
+
+
+}
